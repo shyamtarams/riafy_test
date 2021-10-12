2cca1b426b85a694892402ff82c1753fc9864a96
shyamtarams <shyamkrishnanpv535@gmail.com>
2021-10-12 19:01:25 +0530

    complited first question

--- q1/q1.py
+++ q1/q1.py
@@ -1,8 +1,16 @@
+# open test file
 with open('input.txt') as f:
     contents = f.read()
-    word=contents.split()
-    print(len(word))
-    for i in range(len(word)):
-        print(i)
+    # split each words after space
+    sentence=contents.split()
+    # initialize empty dict to store values
+    counts = {}
+    for i in sentence:
+        if i in counts:
+            counts[i] += 1
+        else:
+            counts[i] = 1
+    print(counts)
 
-   
+
+
